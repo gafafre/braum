7fecfcfa146cb9d8abe52aefe03bb197005e047b
Gabriel Faria de Freitas <gffreitas@edglobo.com.br>
2019-08-15 18:15:40 -0300

    Fix for staticfiles

--- docker/settings.py
+++ docker/settings.py
@@ -126,7 +126,7 @@ STATIC_ROOT = 'static/'
 if os.getcwd() == '/app':
     # Config for staticfiles
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    STATIC_ROOT = 'static'
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
