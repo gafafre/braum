ed8b81c66a67587bdb384935ee2daceca5a342a2
gabriel.freitas <gabriel.f.freitas@outlook.com>
2019-08-15 20:05:26 -0300

    LOGS FOR DEBBUG REVERT THIS COMMIT

--- docker/settings.py
+++ docker/settings.py
@@ -120,13 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 if os.getcwd() == '/app':
     # Config for staticfiles
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'static'
+    STATIC_ROOT = 'staticfiles'
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
